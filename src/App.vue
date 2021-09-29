4a9150c28a997aa6822e03290044b4d063738521
Valley <vaaleyou@gmail.com>
2021-09-29 22:45:18 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container">
+    <Header
+      @toggle-add-task="toggleAddTask"
+      title="Task Tracker"
+      :showAddTask="showAddTask"
+    />
+    <router-view :showAddTask="showAddTask"></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {
+      showAddTask: false,
+    }
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
+  },
+}
+</script>
+
+<style>
+@font-face {
+  font-family: "Mattone-150";
+  src: url("./assets/fonts/Mattone-150.woff2") format("woff2"),
+  url("./assets/fonts/Mattone-150.woff") format("woff");
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Mattone-150', sans-serif;
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 2px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #ffffff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>Version 1.0.0 made by Valentin Musset with ❤️</h3>
+  <router-link to="/">Go Back</router-link>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  inheritAttrs: false, // disable 'non-props' warning
+};
+</script>
